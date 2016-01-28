6fe9afba1663f5bc5095c243884877d290d012b3
xiaozhu kang <xiaozhuk@xilinx.com>
2016-01-28 14:26:10 +0000

    median filter

---
+++ media_filter/README.md
@@ -0,0 +1,51 @@
+median filter Example
+===============================
+
+This is an optimized implementation of a median filter being used to remove noise in images
+targeting execution on an SDAccel supported FPGA acceleration card.
+
+The main algorithm characteristics of this application are
+1. 3x3 median filter
+2. Input image size of 128 x 128
+3. Achieved performance = 0.045ms on image size 128x128
+
+Achieved performance was measured on an Alpha Data ADM-PCI-7v3 card.
+
+Changelog
+----------
+1/27/2016 - Initial Release
+
+Compiling the Application
+---------------------------
+This application is compiled using the SDAccel script mode.
+To compile the application:
+
+```
+sdaccel medianFilter.tcl
+```
+The target board can be changed by editing *medianFilter.tcl*
+
+Executing the Application
+---------------------------
+```
+medianFilter.exe medianFilter.xclbin data/inputImage.bmp
+```
+
+Files in the Example
+---------------------
+Example data
+- data/inputImage.bmp
+
+Application host code
+- medianFilter.cpp
+- oclErrorCodes.cpp
+- bitmap.cpp
+- oclHelper.cpp
+- oclHelper.h
+- bitmap.h
+
+Kernel code
+- krnl_medianFilter.cl
+
+Compilation Script
+- medianFilter.tcl
---
+++ media_filter/medianFilter.tcl
@@ -0,0 +1,45 @@
+# SDAccel Application Example - Median Filter
+# 128 x 128 image size
+
+create_solution -name medianFilter_example -dir . -force
+# Target a Xilinx FPGA board
+add_device -vbnv xilinx:adm-pcie-7v3:1ddr:2.0
+
+# Execution arguments for the application
+set args "$::env(PWD)/data/inputImage.bmp bin_medianFilter.xclbin"
+
+# Host source files
+add_files "medianFilter.cpp"
+add_files "bitmap.cpp"
+add_files "oclErrorCodes.cpp"
+add_files "oclHelper.cpp"
+add_files "bitmap.h"
+set_property file_type "c header files" [get_files "bitmap.h"]
+add_files "oclHelper.h"
+set_property file_type "c header files" [get_files "oclHelper.h"]
+
+# Kernel Definition
+create_kernel median -type clc
+add_files -kernel [get_kernels median] "krnl_medianFilter.cl"
+
+# Define Binary Containers
+create_opencl_binary bin_medianFilter
+set_property region "OCL_REGION_0" [get_opencl_binary bin_medianFilter]
+create_compute_unit -opencl_binary [get_opencl_binary bin_medianFilter] -kernel [get_kernels median] -name cu_medianFilter0
+
+# Compile the design for CPU based emulation
+compile_emulation -flow cpu -opencl_binary [get_opencl_binary bin_medianFilter]
+run_emulation -flow cpu -args $args
+
+# Create estimated resource usage and latency report
+report_estimate
+
+# Compile the design for Hardware Emulation
+compile_emulation -flow hardware -opencl_binary [get_opencl_binary bin_medianFilter]
+run_emulation -flow hardware -args $args
+
+# Compile the design for execution on the FPGA board
+build_system
+
+# Create the board deployment package for the application
+package_system
