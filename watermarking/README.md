3a49933b4e6e906f680047b8aff98dea2077755f
xiaozhu kang <xiaozhuk@xilinx.com>
2016-01-29 12:49:16 -0800

    watermarking example

---
+++ watermarking/README.md
@@ -0,0 +1,51 @@
+watermarking Example
+===============================
+
+This is an optimized implementation of a watermarking application to add watermarking to images
+targeting execution on an SDAccel supported FPGA acceleration card.
+
+The main algorithm characteristics of this application are
+1. watermarking windown 10x10
+2. Input image size of 128 x 128
+3. Achieved performance = 0.163ms on image size 128x128
+
+Achieved performance was measured on an Alpha Data ADM-PCI-7v3 card.
+
+Changelog
+----------
+1/27/2016 - Initial Release
+
+Compiling the Application
+---------------------------
+This application is compiled using the SDAccel script mode.
+To compile the application:
+
+```
+sdaccel watermarking.tcl
+```
+The target board can be changed by editing *watermarking.tcl*
+
+Executing the Application
+---------------------------
+```
+watermarking_example.exe watermarking.xclbin data/inputImage.bmp
+```
+
+Files in the Example
+---------------------
+Example data
+- data/inputImage.bmp
+
+Application host code
+- watermarking.cpp
+- oclErrorCodes.cpp
+- bitmap.cpp
+- oclHelper.cpp
+- oclHelper.h
+- bitmap.h
+
+Kernel code
+- krnl_watermarking.cl
+
+Compilation Script
+- watermarking.tcl
---
+++ watermarking/watermarking.tcl
@@ -0,0 +1,45 @@
+# SDAccel Application Example - watermarking
+# 128 x 128 image size
+
+create_solution -name watermarking_example -dir . -force
+# Target a Xilinx FPGA board
+add_device -vbnv xilinx:adm-pcie-7v3:1ddr:2.0
+
+# Execution arguments for the application
+set args "$::env(PWD)/data/inputImage.bmp bin_watermarking.xclbin"
+
+# Host source files
+add_files "watermarking.cpp"
+add_files "bitmap.cpp"
+add_files "oclErrorCodes.cpp"
+add_files "oclHelper.cpp"
+add_files "bitmap.h"
+set_property file_type "c header files" [get_files "bitmap.h"]
+add_files "oclHelper.h"
+set_property file_type "c header files" [get_files "oclHelper.h"]
+
+# Kernel Definition
+create_kernel applyWatermark -type clc
+add_files -kernel [get_kernels applyWatermark] "krnl_watermarking.cl"
+
+# Define Binary Containers
+create_opencl_binary bin_watermarking
+set_property region "OCL_REGION_0" [get_opencl_binary bin_watermarking]
+create_compute_unit -opencl_binary [get_opencl_binary bin_watermarking] -kernel [get_kernels applyWatermark] -name cu_watermarking0
+
+# Compile the design for CPU based emulation
+compile_emulation -flow cpu -opencl_binary [get_opencl_binary bin_watermarking]
+run_emulation -flow cpu -args $args
+
+# Create estimated resource usage and latency report
+report_estimate
+
+# Compile the design for Hardware Emulation
+#compile_emulation -flow hardware -opencl_binary [get_opencl_binary bin_watermarking]
+#run_emulation -flow hardware -args $args
+
+# Compile the design for execution on the FPGA board
+#build_system
+
+# Create the board deployment package for the application
+#package_system
