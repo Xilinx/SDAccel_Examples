76eeb0061e6241f8d4944fdb861584c987a43f0a
fernando martinez <vallina@xilinx.com>
2016-07-29 17:44:36 -0700

    application moved to new location

--- vdotprod/compile_fpga_zc706.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-##this line sets the path to the dsa for zc706 device and also invokes xocc on the kernel
-xocc --xdevice xilinx:zc706:uart:1.0 --xp "prop:solution.device_repo_paths=$PWD/dsa" krnl_vdotprod.cl -o vdotprod.xclbin
