d93050a991900103df141e4750e0939d43f2f965
Spenser Gilliland <spenser@gillilanding.com>
2016-09-02 16:52:00 -0700

    Revert "build for ku3 only"
    This reverts commit 815ba2b22ade3893a5720c8de3dc187de1a2be7e.

--- utility/boards.mk
+++ utility/boards.mk
@@ -5,8 +5,7 @@ XOCCFLAGS:=-g --xp "param:compiler.preserveHlsOutput=1" --xp "param:compiler.gen
 CLC:=$(XILINX_SDACCEL)/bin/xocc
 
 # Devices per Architecture
-#remove xilinx:adm-pcie-7v3:1ddr:2.1 to save debug time
-X86_DEVICES:= xilinx:adm-pcie-ku3:2ddr:2.1
+X86_DEVICES:= xilinx:adm-pcie-7v3:1ddr:2.1 xilinx:adm-pcie-ku3:2ddr:2.1
 X86_CXX:=$(XILINX_SDACCEL)/lnx64/tools/gcc/bin/g++
 
 POWER_DEVICES:= xilinx:adm-pcie-7v3:1ddr-ppc64le:2.1
