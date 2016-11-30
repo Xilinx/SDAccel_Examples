b641954407eddadae55c6c2f3c46a0bb35176092
Spenser Gilliland <spenser@gillilanding.com>
2016-11-30 14:09:19 -0800

    gmem_2banks_ocl: initial commit

---
+++ getting_started/kernel_to_gmem/gmem_2banks_ocl/README.md
@@ -0,0 +1,110 @@
+Global Memory Two Banks Example Example 
+======================
+
+This README file contains the following sections:
+
+1. OVERVIEW
+2. SOFTWARE TOOLS AND SYSTEM REQUIREMENTS
+3. DESIGN FILE HIERARCHY
+4. INSTALLATION AND OPERATING INSTRUCTIONS
+5. SUPPORT
+6. LICENSE
+7. ACKNOWLEDGEMENTS
+8. REVISION HISTORY
+
+
+## 1. OVERVIEW
+This example of 2ddr to demonstrate on how to use 2ddr DSA. How to create buffers in each DDR.
+
+### KEY CONCEPTS
+
+- Multiple Banks
+
+### KEYWORDS
+- max_memory_ports
+- param:compiler.enablemap_connect
+- misc:map_connect
+- cl_mem_ext_ptr_t
+- XCL_MEM_DDR_BANK0
+- XCL_MEM_DDR_BANK1
+- XCL_MEM_DDR_BANKx
+- CL_MEM_EXT_PTR_XILINX
+
+## 2. SOFTWARE AND SYSTEM REQUIREMENTS
+Board | Software Version
+------|---------
+Alpha Data ADM-PCIE-7V3|SDAccel 2016.2
+Alpha Data ADM-PCIE-KU3|SDAccel 2016.2
+
+
+## 3. DESIGN FILE HIERARCHY
+Application code is located in the src directory. Accelerator binary files will be compiled to the xclbin directory. The xclbin directory is required by the Makefile and its contents will be filled during compilation.
+
+## 4. INSTALLATION AND OPERATING INSTRUCTIONS
+### Compilation for x86 based servers
+1. make all
+
+	Launches compilation both emulation and board execution modes of the application for all supported boards.To limit or change the boards for compilation, edit the utility/boards.mk file located at the top level of this repository.
+
+2. Executing emulation flows
+Set the following environment variable to create a emconfig.json file
+```
+export LD_LIBRARY_PATH=$XILINX_SDACCEL/runtime/lib/x86_64/:$LD_LIBRARY_PATH
+export XCL_EMULATION_MODE=<sw_emu|hw_emu>
+emconfigutil --xdevice 'xilinx:adm-pcie-ku3:2ddr:3.1' --nd 1
+```
+Run the application as
+```
+./host
+```
+The choices for emulation flow are
+		- sw_emu = software emulation
+		- hw_emu = hardware emulation
+*NOTE:* The software emulation flow is a functional correctness check only. It does not estimate the performance of the application in hardware.
+The hardware emulation flow is a cycle accurate simulation of the hardware generated for the application. As such, it is expected for this simulation to take a long time.
+It is recommended that for this example the user skips running hardware emulation or modifies the example to work on a reduced data set.
+3. Running the application on a board hosted by Nimbix from a remote system
+
+	Install the Xilinx Runtime for board in Nimbix using the xbinst utility in SDAccel
+```
+xbinst -x 'xilinx:adm-pcie-ku3:2ddr:3.1' -d .
+mv xbinst/pkg/pcie/runtime . 
+rm -rf xbinst
+```
+Run the nimbix-run.py script provided in the utility/nimbix directory at the top level of this repository
+```
+../../../utility/nimbix/nimbix-run.py ./host
+```
+
+	Only the Alpha Data ADM-PCIE-KU3 card is supported by this method.
+4. Running the application from within Nimbix
+
+	Follow the instructions in the [Nimbix Getting Started Guide][]
+
+### Compilation for IBM POWER servers
+1. View the SuperVessel [Walkthrough Video][]
+
+2. make all-POWER
+	Launches compilation both emulation and board execution modes of the application for all supported boards.
+
+## 5. SUPPORT
+For more information about SDAccel check the [SDAccel User Guides][]
+
+For questions and to get help on this project or your own projects, visit the [SDAccel Forums][].
+
+## 6. LICENSE
+The source for this project is licensed under the [3-Clause BSD License][]
+
+## 7. ACKNOWLEDGEMENTS
+This example is written by developers at
+- [Xilinx](http://www.xilinx.com)
+
+## 8. REVISION HISTORY
+Date | README Version | Description
+-----|----------------|------------
+DEC2016|1.0|Initial Xilinx Release
+[3-Clause BSD License]: LICENSE.md
+[SDAccel Forums]: https://forums.xilinx.com/t5/SDAccel/bd-p/SDx
+[SDAccel User Guides]: http://www.xilinx.com/support/documentation-navigation/development-tools/software-development/sdaccel.html?resultsTablePreSelect=documenttype:SeeAll#documentation
+[Nimbix Getting Started Guide]: http://www.xilinx.com/member/sdaccel_ea_40hr/Getting_Started_with_SDAccel_on_Nimbix_cloud.pdf
+[Walkthrough Video]: http://bcove.me/6pp0o482
---
+++ getting_started/kernel_to_gmem/gmem_2banks_ocl/src/host.cpp
@@ -0,0 +1,162 @@
+/*******************************************************************************
+Copyright (c) 2016, Xilinx, Inc.
+All rights reserved.
+ 
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+ 
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+ 
+ 
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+ 
+ 
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+ 
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+
+#include <iostream>
+
+//Includes
+#include "xcl.h"
+#include "bitmap.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <input bitmap> <golden bitmap>" << std::endl;
+        return EXIT_FAILURE ;
+    }
+    const char* bitmapFilename = argv[1];
+    const char* goldenFilename = argv[2];
+  
+    //Read the input bit map file into memory
+    BitmapInterface image(bitmapFilename);
+    bool result = image.readBitmapFile() ;
+    if (!result)
+    {
+        std::cout << "ERROR:Unable to Read Input Bitmap File "<< bitmapFilename << std::endl;
+        return EXIT_FAILURE ;
+    }
+
+    //Read the golden bit map file into memory
+    BitmapInterface goldenImage(goldenFilename);
+    result = goldenImage.readBitmapFile() ;
+    if (!result)
+    {
+        std::cout << "ERROR:Unable to Read Golden Bitmap File "<< goldenFilename << std::endl;
+        return EXIT_FAILURE ;
+    }
+
+    int width = image.getWidth() ;
+    int height = image.getHeight() ;
+   
+    //Allocate Memory in Host Memory 
+    int image_size_bytes = image.numPixels() * sizeof(int); 
+    int* outImage = (int*)(malloc(image_size_bytes)) ;
+    if (outImage == NULL)
+    {
+        std::cout << "Unable to allocate host memory!" << std::endl ;
+        return EXIT_FAILURE ;
+    }
+
+//OPENCL HOST CODE AREA START
+   //Create Program and Kernels
+    xcl_world world = xcl_world_single();
+    cl_program program = xcl_import_binary(world, "apply_watermark");
+    cl_kernel krnl_applyWatermark = xcl_get_kernel(program, "apply_watermark");
+
+    // For Allocating Buffer to specific Global Memory Bank, user has to use cl_mem_ext_ptr_t
+    // and provide the Banks
+    //
+    cl_mem_ext_ptr_t inExt, outExt;  // Declaring two extensions for both buffers
+    inExt.flags  = XCL_MEM_DDR_BANK0; // Specify Bank0 Memory for input memory
+    outExt.flags = XCL_MEM_DDR_BANK1; // Specify Bank1 Memory for output Memory
+    inExt.obj = 0   ; outExt.obj = 0; // Setting Obj and Param to Zero
+    inExt.param = 0 ; outExt.param = 0; 
+
+    int err;
+    //Allocate Buffer in Bank0 of Global Memory for Input Image using Xilinx Extension
+    cl_mem buffer_inImage = clCreateBuffer(world.context, CL_MEM_READ_ONLY | CL_MEM_EXT_PTR_XILINX,
+            image_size_bytes, &inExt, &err);
+    if (err != CL_SUCCESS){
+        std::cout << "Error: Failed to allocate device Memory" << std::endl;
+        return EXIT_FAILURE;
+    }
+    //Allocate Buffer in Bank1 of Global Memory for Input Image using Xilinx Extension
+    cl_mem buffer_outImage = clCreateBuffer(world.context, CL_MEM_WRITE_ONLY | CL_MEM_EXT_PTR_XILINX,
+            image_size_bytes, &outExt, NULL);
+    if (err != CL_SUCCESS){
+        std::cout << "Error: Failed to allocate device Memory" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    //Copy input Image to device global memory
+    xcl_memcpy_to_device(world,buffer_inImage,image.bitmap(),image_size_bytes);
+   
+    //Set the Kernel Arguments
+    xcl_set_kernel_arg(krnl_applyWatermark,0,sizeof(cl_mem),&buffer_inImage);
+    xcl_set_kernel_arg(krnl_applyWatermark,1,sizeof(cl_mem),&buffer_outImage);
+    xcl_set_kernel_arg(krnl_applyWatermark,2,sizeof(int),&width);
+    xcl_set_kernel_arg(krnl_applyWatermark,3,sizeof(int),&height);
+    
+    //Launch the Kernel
+    xcl_run_kernel3d(world,krnl_applyWatermark,1,1,1);
+
+    //Copy Result from Device Global Memory to Host Local Memory
+    xcl_memcpy_from_device(world, outImage, buffer_outImage,image_size_bytes);
+    clFinish(world.command_queue);
+
+    //Release Device Memories and Kernels
+    clReleaseMemObject(buffer_inImage);
+    clReleaseMemObject(buffer_outImage);
+    clReleaseKernel(krnl_applyWatermark);
+    xcl_release_world(world);
+//OPENCL HOST CODE AREA END
+
+
+    //Compare Golden Image with Output image
+    bool match = 0;
+    if ( image.getHeight() != goldenImage.getHeight() || image.getWidth() != goldenImage.getWidth()){
+        match = 1;
+    }else{
+        int* goldImgPtr = goldenImage.bitmap();
+        for (int i = 0 ; i < image.numPixels(); i++){
+            if (outImage[i] != goldImgPtr[i]){
+                match = 1;
+                printf ("Pixel %d Mixmatch Output %x and Expected %x \n", i, outImage[i], goldImgPtr[i]);
+                break;
+            }
+        }
+    }
+    
+    // Write the final image to disk
+    image.writeBitmapFile(outImage);
+
+    free(outImage) ;
+    if (match){
+        std::cout << "TEST FAILED." << std::endl; 
+        return EXIT_FAILURE;
+    }
+    std::cout << "TEST PASSED." << std::endl;
+    return EXIT_SUCCESS;
+
+}
