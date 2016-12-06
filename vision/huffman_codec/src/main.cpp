102ffc05fcc68a3486e20a5a60c4e8d157707112
Xiaozhu Kang <xiaozhu.kang@xilinx.com>
2016-12-05 16:48:41 -0800

    Update main.cpp

--- vision/huffman_codec/src/main.cpp
+++ vision/huffman_codec/src/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[]) {
   	LogInfo("Chosen bitmap file is %s",strBitmapFP.c_str());
 	LogInfo("Chosen kernel file is %s", strKernelFullPath.c_str());
 	LogInfo("Chosen Platform = %s, Device Name: %s, Device Index: [%d]", strPlatformName.c_str(), strDeviceName.c_str(), idxSelectedDevice);
-	HuffmanOptimized huffman(strPlatformName, strDeviceName, idxSelectedDevice, strKernelFullPath, strBitmapFP);
+	HuffmanOptimized huffman();//strPlatformName, strDeviceName, idxSelectedDevice, strKernelFullPath, strBitmapFP);
 
 	//LogInfo("Perform some unit tests before the actual image decode, encode");
 	//unit_test_codec(&huffman);
