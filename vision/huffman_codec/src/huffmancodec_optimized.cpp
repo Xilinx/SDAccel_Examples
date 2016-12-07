8241ad22ccc327e6d02ae6dde738a4f2f55df798
Xiaozhu Kang <xiaozhu.kang@xilinx.com>
2016-12-06 17:46:10 -0800

    Update huffmancodec_optimized.cpp

--- vision/huffman_codec/src/huffmancodec_optimized.cpp
+++ vision/huffman_codec/src/huffmancodec_optimized.cpp
@@ -64,14 +64,14 @@ HuffmanOptimized::HuffmanOptimized(const string& vendor_name,
 			   const string& strBitmapFP)
 {
 	//store path to input bitmap
-	//m_strBitmapFP = strBitmapFP;
+	m_strBitmapFP = strBitmapFP;
 
 
 	//list available platforms
-	//xcl_list_platforms();
+	xcl_list_platforms();
 
 	//list available devices
-/*	if(xcl_list_devices(vendor_name.c_str()) > 0) {
+	if(xcl_list_devices(vendor_name.c_str()) > 0) {
 
 		//get device by name
 		assert(xcl_world_vendor_devtype(vendor_name.c_str(), CL_DEVICE_TYPE_ACCELERATOR, m_world));
@@ -79,7 +79,7 @@ HuffmanOptimized::HuffmanOptimized(const string& vendor_name,
 
 	//kernels
     m_clKernelHuffmanEncoder  = xcl_import_binary(m_world, strKernelFP.c_str(), "encode");
-    m_clKernelHuffmanDecoder  = xcl_import_binary(m_world, strKernelFP.c_str(), "decode");*/
+    m_clKernelHuffmanDecoder  = xcl_import_binary(m_world, strKernelFP.c_str(), "decode");
 
 }
 
