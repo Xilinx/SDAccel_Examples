ba82449f80eb632c65da2f6fd02dd3854e8d1b27
fernando martinez <vallina@xilinx.com>
2016-12-09 16:39:05 -0800

    Updates to the generator to create more verbose documentation

--- utility/readme_gen/readme_gen.py
+++ utility/readme_gen/readme_gen.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from sys import argv
 import json
+import os
+import subprocess
 
-DSA = 'xilinx:adm-pcie-ku3:2ddr:3.1'
-VERSION = 'SDAccel 2016.2'
+DSA = 'xilinx:adm-pcie-ku3:2ddr:3.2'
+VERSION = 'SDAccel 2016.3'
+DEVICES = {'Alpha Data ADM-PCIE-7V3':'xilinx:adm-pcie-7v3:1ddr:3.2', 'Alpha Data ADM-PCIE-KU3':'xilinx:adm-pcie-ku3:2ddr:3.2', 'Xilinx KU115':'xilinx:xil-accel-rd-ku115:4ddr-xpr:3.2'}
 
 def header(target,data):
     target.write(data["example"])
@@ -11,13 +14,28 @@ def header(target,data):
     target.write("======================\n\n")
     target.write("This README file contains the following sections:\n\n")
     target.write("1. OVERVIEW\n")
-    target.write("2. SOFTWARE TOOLS AND SYSTEM REQUIREMENTS\n")
-    target.write("3. DESIGN FILE HIERARCHY\n")
-    target.write("4. INSTALLATION AND OPERATING INSTRUCTIONS\n")
-    target.write("5. SUPPORT\n")
-    target.write("6. LICENSE\n")
-    target.write("7. ACKNOWLEDGEMENTS\n")
-    target.write("8. REVISION HISTORY\n\n\n")
+    target.write("2. HOW TO DOWLOAD THE REPOSITORY\n")
+    target.write("3. SOFTWARE TOOLS AND SYSTEM REQUIREMENTS\n")
+    target.write("4. DESIGN FILE HIERARCHY\n")
+    target.write("5. COMPILATION AND EXECUTION ON X86 SERVERS\n")
+    target.write("6. COMPILATION AND EXECUTION ON IBM POWER SERVERS\n")
+    target.write("7. SUPPORT\n")
+    target.write("8. LICENSE AND CONTRIBUTING TO THE REPOSITORY\n")
+    target.write("9. ACKNOWLEDGEMENTS\n")
+    target.write("10. REVISION HISTORY\n\n\n")
+    return
+
+def download(target):
+    target.write("## 2. HOW TO DOWNLOAD THE REPOSITORY\n")
+    target.write("To get a local copy of the SDAccel example repository, clone this repository to the local system with the following command:\n")
+    target.write("```\n")
+    target.write("git clone https://github.com/Xilinx/SDAccel_Examples examples\n")
+    target.write("```\n")
+    target.write("where examples is the name of the directory where the repository will be stored on the local system.")
+    target.write("This command needs to be executed only once to retrieve the latest version of all SDAccel examples. The only required software is a local installation of git.\n\n")
+    target.write("*NOTE: If the destination of the repository is the user home directory in the Nimbix SDAccel developer instance, the command will need to be executed for every ")
+    target.write("session. Contents of the user home directory in the Nimbix SDAccel developer instance are not preserved between user sessions. Only files placed in the /data ")
+    target.write("directory are kept between SDAccel development sessions.*\n")
     return
 
 def overview(target,data):
@@ -58,94 +76,162 @@ def overview(target,data):
     return
 
 def requirements(target,data):
-    target.write("## 2. SOFTWARE AND SYSTEM REQUIREMENTS\n")
-    target.write("Board | Software Version\n")
-    target.write("------|---------\n")
+    target.write("## 3. SOFTWARE AND SYSTEM REQUIREMENTS\n")
+    target.write("Board | Device Name | Software Version\n")
+    target.write("------|-------------|---------\n")
     for board in data["board"]:
         target.write(board)
+        target.write("|")
+        target.write(DEVICES[board])
         target.write("|")
         for version in VERSION:
             target.write(version)
         target.write("\n")
     target.write("\n\n")
+    target.write("Board targeted by default = ***Alpha Data ADM-PCIE-KU3***\n\n")
+    target.write("*NOTE:* The board/device used for compilation can be changed by adding the DEVICES variable to the make command as shown below\n")
+    target.write("```\n")
+    target.write("make DEVICES=<device name>\n")
+    target.write("```\n")
+    target.write("where the *DEVICES* variable accepts either 1 device from the table above or a comma separated list of device names.\n")
     return
 
 def hierarchy(target):
-    target.write("## 3. DESIGN FILE HIERARCHY\n")
+    target.write("## 4. DESIGN FILE HIERARCHY\n")
     target.write("Application code is located in the src directory. ")
     target.write("Accelerator binary files will be compiled to the xclbin directory. ")
-    target.write("The xclbin directory is required by the Makefile and its contents will be filled during compilation.\n\n")
+    target.write("The xclbin directory is required by the Makefile and its contents will be filled during compilation. A listing of all the files ")
+    target.write("in this example is shown below\n\n")
+    target.write("```\n")
+    tree_cmd = ['tree']
+    proc = subprocess.Popen(tree_cmd,stdout=subprocess.PIPE)
+    output = proc.communicate()[0]
+    target.write(output)
+    target.write("```\n")
+    target.write("\n")
     return
 
 def compilation(target,data):
-    target.write("## 4. INSTALLATION AND OPERATING INSTRUCTIONS\n")
-    target.write("### Compilation for x86 based servers\n")
-    target.write("1. make all\n")
-    target.write("\n\tLaunches compilation both emulation and board execution modes of the application for all supported boards.")
-    target.write("To limit or change the boards for compilation, edit the utility/boards.mk file located at the top level of this repository.\n\n")
-    target.write("2. Executing emulation flows\n")
-    target.write("Set the following environment variable to create a emconfig.json file\n")
+    target.write("## 5. COMPILATION AND EXECUTION FOR X86 SERVERS\n")
+    target.write("### Compiling for Application Emulation\n")
+    target.write("As part of the capabilities available to an application developer, SDAccel includes environments to test the correctness of an application at both a software functional level and a hardware emulated level.\n")
+    target.write("These modes, which are named sw_emu and hw_emu, allow the developer to profile and evaluate the performance of a design before compiling for board execution.\n")
+    target.write("It is recommended that all applications are executed in at least the sw_emu mode before being compiled and executed on an FPGA board.\n")
     target.write("```\n")
-    target.write("export LD_LIBRARY_PATH=$XILINX_SDACCEL/runtime/lib/x86_64/:$LD_LIBRARY_PATH\n")
+    target.write("make TARGETS=<sw_emu|hw_emu> all\n")
+    target.write("```\n")
+    target.write("where\n")
+    target.write("```\n")
+    target.write("\tsw_emu = software emulation\n")
+    target.write("\thw_emu = hardware emulation\n")
+    target.write("```\n")
+    target.write("*NOTE:* The software emulation flow is a functional correctness check only. It does not estimate the performance of the application in hardware.\n")
+    target.write("The hardware emulation flow is a cycle accurate simulation of the hardware generated for the application. As such, it is expected for this simulation to take a long time.\n")
+    target.write("It is recommended that for this example the user skips running hardware emulation or modifies the example to work on a reduced data set.\n")
+    target.write("### Executing Emulated Application \n")
+    target.write("***Recommended Execution Flow for Example Applications in Emulation*** \n\n")
+    target.write("The makefile for the application can directly executed the application with the following command:\n")
+    target.write("```\n")
+    target.write("make TARGETS=<sw_emu|hw_emu> check\n\n")
+    target.write("```\n")
+    target.write("where\n")
+    target.write("```\n")
+    target.write("\tsw_emu = software emulation\n")
+    target.write("\thw_emu = hardware emulation\n")
+    target.write("```\n")
+    target.write("If the application has not been previously compiled, the check makefile rule will compile and execute the application in the emulation mode selected by the user.\n\n")
+    target.write("***Alternative Execution Flow for Example Applications in Emulation*** \n\n")
+    target.write("An emulated application can also be executed directly from the command line without using the check makefile rule as long as the user environment has been properly configured.\n")
+    target.write("To manually configure the environment to run the application, set the following\n")
+    target.write("```\n")
+    target.write("export LD_LIBRARY_PATH=$XILINX_SDX/runtime/lib/x86_64/:$LD_LIBRARY_PATH\n")
     target.write("export XCL_EMULATION_MODE=")
     try:
       if not data['xcl']:
-        target.write('true')
+            target.write('true')
       else:
-        target.write('<sw_emu|hw_emu>')
+            target.write('<sw_emu|hw_emu>')
     except:
-      target.write('<sw_emu|hw_emu>')
+        target.write('<sw_emu|hw_emu>')
     target.write('\n')
     target.write("emconfigutil --xdevice '")
     target.write(DSA)
     target.write("' --nd 1\n")
     target.write("```\n")
-    target.write("Run the application as\n")
+    target.write("Once the environment has been configured, the application can be executed by\n")
     target.write("```\n")
     target.write(data["em_cmd"])
     target.write("\n```\n")
-    target.write("The choices for emulation flow are\n")
-    target.write("\t\t- sw_emu = software emulation\n")
-    target.write("\t\t- hw_emu = hardware emulation\n")
-    target.write("*NOTE:* The software emulation flow is a functional correctness check only. It does not estimate the performance of the application in hardware.\n")
-    target.write("The hardware emulation flow is a cycle accurate simulation of the hardware generated for the application. As such, it is expected for this simulation to take a long time.\n")
-    target.write("It is recommended that for this example the user skips running hardware emulation or modifies the example to work on a reduced data set.\n")
-    target.write("3. Running the application on a board hosted by Nimbix from a remote system\n")
-    target.write("\n\tInstall the Xilinx Runtime for board in Nimbix using the xbinst utility in SDAccel\n")
+    target.write("This is the same command executed by the check makefile rule\n")
+    target.write("### Compiling for Application Execution in the FPGA Accelerator Card\n")
+    target.write("The command to compile the application for execution on the FPGA acceleration board is\n")
     target.write("```\n")
-    target.write("xbinst -x '")
-    target.write(DSA)
-    target.write("' -d .\n")
-    target.write("mv xbinst/pkg/pcie/runtime . \n")
-    target.write("rm -rf xbinst\n")
+    target.write("make all\n")
     target.write("```\n")
-    target.write("Run the nimbix-run.py script provided in the utility/nimbix directory at the top level of this repository\n")
+    target.write("The default target for the makefile is to compile for hardware. Therefore, setting the TARGETS option is not required.\n")
+    target.write("*NOTE:* Compilation for application execution in hardware generates custom logic to implement the functionality of the kernels in an application.")
+    target.write("It is typical for hardware compile times to range from 30 minutes to a couple of hours.\n")
+    target.write("### Executing the Application in the FPGA Accelerator Card\n")
+    target.write("The developer instance hosting the SDAccel tools on Nimbix is not directly connected to an FPGA accelerator card.\n")
+    target.write("FPGA Accelerator cards are available as part of the SDAccel Runtime application. There are several ways of executing an application on the available cards:\n\n")
+    target.write("***Submit the application from the developer to runtime instances (recommended flow)***\n")
+    target.write("* Create a credentials file for the runtime machine based on your Nimbix username and API key. For more information on how to obtain the API key, refer to ")
+    target.write("[Nimbix Application Submission README][]. The credentials file ( ~/.nimbix_creds.json ) should look like\n")
+    target.write("```\n")
+    target.write("{\n")
+    target.write("\t\"username\": \"<username>\",\n")
+    target.write("\t\"api-key\": \"<apikey>\"\n")
+    target.write("}\n")
+    target.write("```\n\n")
+    target.write("where the values for username and apikey have been set to the values from your Nimbix account.\n\n")
+    target.write("*NOTE:* The home directory of a SDAccel developer instance is not persistent across sessions. Only files stored in the /data directory are kept between sessions.")
+    target.write("It is recommended that a copy of the nimbix_creds.json file be stored in the /data directory and copied to the appropriate location in the home directory ")
+    target.write("at the start of each development session.\n")
+    target.write("* Launch the application\n")
+    target.write("```\n")
+    target.write("make check\n")
+    target.write("```\n")
+    target.write("***Launch the application from a remote system outside of the Nimbix environment***\n")
+    target.write("* Follow the instructions in [Nimbix Application Submission README][]\n\n")
+    target.write("* Use the following command to launch the application from the users terminal (on a system outside of the Nimbix environment)\n")
     target.write("```\n")
     target.write(data["hw_cmd"])
     target.write("\n")
     target.write("```\n\n")
-    target.write("\tOnly the Alpha Data ADM-PCIE-KU3 card is supported by this method.\n")
-    target.write("4. Running the application from within Nimbix\n")
-    target.write("\n\tFollow the instructions in the [Nimbix Getting Started Guide][]\n")
-    target.write("\r\n### Compilation for IBM POWER servers\n")
-    target.write("1. View the SuperVessel [Walkthrough Video][]\n\n")
-    target.write("2. make all-POWER\n")
-    target.write("\tLaunches compilation both emulation and board execution modes of the application for all supported boards.\n")
+    target.write("***Copy the application files from the Developer to Runtime instances on Nimbix***\n")
+    target.write("* Copy the application *.exe file and xclbin directory to the /data directory\n")
+    target.write("* Launch the application using the Nimbix web interface as described in [Nimbix Getting Started Guide][]\n")
+    target.write("* Make sure that the application launch options in the Nimbix web interface reflect the applications command line syntax\n")
+    target.write("```\n")
+    target.write(data["em_cmd"])
+    target.write("\n")
+    target.write("```\n")
+    return
+
+def power(target):
+    target.write("\r\n## 6. COMPILATION AND EXECUTION FOR IBM POWER SERVERS\n")
+    target.write("View the SuperVessel [Walkthrough Video][] to become familiar with the environment.\n\n")
+    target.write("Compile the application with the following command\n")
+    target.write("```\n")
+    target.write("make ARCH=POWER all\n")
+    target.write("```\n")
     return
 
 def support(target):
-    target.write("\r\n## 5. SUPPORT\n")
+    target.write("\r\n## 7. SUPPORT\n")
     target.write("For more information about SDAccel check the [SDAccel User Guides][]\n\n")
     target.write("For questions and to get help on this project or your own projects, visit the [SDAccel Forums][].\n")
     return
 
 def license(target):
-    target.write("\r\n## 6. LICENSE\n")
-    target.write("The source for this project is licensed under the [3-Clause BSD License][]\n")
+    target.write("\r\n## 8. LICENSE AND CONTRIBUTING TO THE REPOSITORY\n")
+    target.write("The source for this project is licensed under the [3-Clause BSD License][]\n\n")
+    target.write("To contribute to this project, follow the guidelines in the [Repository Contribution README][]\n")
+
     return
 
 def ack(target,data):
-    target.write("\r\n## 7. ACKNOWLEDGEMENTS\n")
+    target.write("\r\n## 9. ACKNOWLEDGEMENTS\n")
     target.write("This example is written by developers at\n")
     for contributor in data["contributors"]:
         target.write("- [")
@@ -156,7 +242,7 @@ def ack(target,data):
     return
 
 def revision(target,data):
-    target.write("\r\n## 8. REVISION HISTORY\n")
+    target.write("\r\n## 10. REVISION HISTORY\n")
     target.write("Date | README Version | Description\n")
     target.write("-----|----------------|------------\n")
     for rev in data["revision"]:
@@ -166,14 +252,56 @@ def revision(target,data):
         target.write("|")
         target.write(rev["description"])
         target.write("\n")
+    target.write("\n")
     return
 
+def dirTraversal(stop_file):
+    last_loc = os.getcwd()
+    current_loc = os.getcwd()
+    stop_search = None
+    level_count = 0
+    while stop_search is None and current_loc:
+        pruned = False
+        for root, dirs, files in os.walk(current_loc):
+            if not pruned:
+                try:
+                    del dirs[dirs.index(os.path.basename(last_loc))]
+                    pruned = True
+                except ValueError:
+                    pass
+            if stop_file in files:
+                stop_search = os.path.join(root,stop_file)
+                break
+        last_loc = current_loc
+        current_loc = os.path.dirname(last_loc)
+        level_count += 1
+    return level_count
+
+def relativeTree(levels):
+    s = '../'
+    for i in range(0,levels-2):
+        s += '../'
+    return s
+
 def footer(target):
-    target.write("[3-Clause BSD License]: LICENSE.md\n")
+    relativeLevels = dirTraversal("LICENSE.txt")
+    s = relativeTree(relativeLevels)
+    licPath = s + 'LICENSE.txt'
+    contribPath = s + 'CONTRIBUTING.md'
+    nimbixPath = s + 'utility/nimbix/README.md'
+    target.write("[3-Clause BSD License]:")
+    target.write(licPath)
+    target.write("\n")
     target.write("[SDAccel Forums]: https://forums.xilinx.com/t5/SDAccel/bd-p/SDx\n")
     target.write("[SDAccel User Guides]: http://www.xilinx.com/support/documentation-navigation/development-tools/software-development/sdaccel.html?resultsTablePreSelect=documenttype:SeeAll#documentation\n")
-    target.write("[Nimbix Getting Started Guide]: http://www.xilinx.com/member/sdaccel_ea_40hr/Getting_Started_with_SDAccel_on_Nimbix_cloud.pdf\n")
-    target.write("[Walkthrough Video]: http://bcove.me/6pp0o482")
+    target.write("[Nimbix Getting Started Guide]: http://www.xilinx.com/support/documentation/sw_manuals/xilinx2016_2/ug1240-sdaccel-nimbix-getting-started.pdf\n")
+    target.write("[Walkthrough Video]: http://bcove.me/6pp0o482\n")
+    target.write("[Nimbix Application Submission README]:")
+    target.write(nimbixPath)
+    target.write("\n")
+    target.write("[Repository Contribution README]:")
+    target.write(contribPath)
+    target.write("\n")
     return
 
 # Get the argument from the description
@@ -193,9 +321,11 @@ print "Generating the README for %r" % data["example"]
 target = open("README.md","w")
 header(target,data)
 overview(target,data)
+download(target)
 requirements(target,data)
 hierarchy(target)
 compilation(target,data)
+power(target)
 support(target)
 license(target)
 ack(target,data)
