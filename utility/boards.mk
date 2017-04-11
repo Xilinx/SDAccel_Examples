36149125ae4eee54e5a81ff04206e6e2d75d8b09
Heera Nand <heeran@xilinx.com>
2017-04-11 11:52:18 +0530

    Updated Makefile for versionless DSA

--- utility/boards.mk
+++ utility/boards.mk
@@ -36,7 +36,7 @@ ifeq ($(ARCH),POWER)
 DEVICES:= xilinx:adm-pcie-7v3:1ddr-ppc64le:2.1
 CXX:=$(XILINX_SDACCEL)/gnu/ppc64le/4.9.3/lnx64/bin/powerpc64le-linux-gnu-g++
 else
-DEVICES:= xilinx:xil-accel-rd-ku115:4ddr-xpr:3.3
+DEVICES:= xilinx:xil-accel-rd-ku115:4ddr-xpr
 CXX:=$(XILINX_SDACCEL)/Vivado_HLS/lnx64/tools/gcc/bin/g++
 endif
 
