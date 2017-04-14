642bdd8e97aa4be9b0b0f9a97f917f51c91b71b8
Heera Nand <heeran@xilinx.com>
2017-04-14 13:10:23 +0530

    Added new example "loop_fusion_c"

---
+++ getting_started/kernel_opt/loop_fusion_c/src/host.cpp
@@ -0,0 +1,197 @@
+/**********
+Copyright (c) 2017, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+#include "xcl2.hpp"
+
+#include <algorithm>
+#include <cstdio>
+#include <fstream>
+#include <random>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using std::default_random_engine;
+using std::equal;
+using std::generate;
+using std::ifstream;
+using std::ignore;
+using std::make_pair;
+using std::numeric_limits;
+using std::uniform_int_distribution;
+using std::vector;
+
+void find_nearest_neighbor(vector<int> &out, const int dim,
+                           const vector<int> &search_points,
+                           const vector<int> &points, const int len);
+
+int gen_random() {
+  static default_random_engine e;
+  static uniform_int_distribution<int> dist(0, 100);
+
+  return dist(e);
+}
+
+void verify(vector<int> gold, vector<int> test) {
+  bool match = true;
+  match = equal(begin(gold), end(gold), begin(test));
+
+  if (!match) {
+    printf("\n TEST FAILED\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void
+print_point(vector<int>& point) {
+  for (int element : point) printf("%d ", element);
+  printf("\n");
+}
+
+// This example illustrates the algorithm of nearest neighbor search for a given
+// point (x, y) from a list of points.
+int main(int argc, char **argv) {
+
+  static const int num_points = 512;
+  static const int num_dims = 2;
+
+  vector<int> data(num_points * num_dims);
+  vector<int> input(1 * num_dims);
+  vector<int> out(input.size());
+  generate(begin(data), end(data), gen_random);
+  generate(begin(input), end(input), gen_random);
+
+  printf("Number Of Points: %d\n", num_points);
+  printf("Dimensions: %d\n", num_dims);
+  printf("\nInput Point:      ");
+  print_point(input);
+
+  vector<int> gold;
+  find_nearest_neighbor(gold, num_dims, input, data, num_points);
+  printf("Nearest Neighbor: ");
+  print_point(gold);
+
+  size_t array_size_bytes = num_points * num_dims * sizeof(int);
+  std::vector<cl::Device> devices = xcl::get_xil_devices();
+  cl::Device device = devices[0];
+
+  cl::Context context(device);
+  cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+  std::string device_name = device.getInfo<CL_DEVICE_NAME>(); 
+
+  //Create Program 
+  cl::Program::Binaries bins = xcl::import_binary(device_name,"nearest_neighbor");
+  devices.resize(1);
+  cl::Program program(context, devices, bins);
+
+  cl::Buffer buffer_out   (context, CL_MEM_WRITE_ONLY,num_dims * sizeof(int));
+  cl::Buffer buffer_points(context, CL_MEM_READ_ONLY, array_size_bytes);
+  cl::Buffer buffer_in    (context, CL_MEM_READ_ONLY, num_dims * sizeof(int));
+
+  // copy the input arrays to memories allocated on the device
+  q.enqueueWriteBuffer(buffer_points,CL_TRUE,0,array_size_bytes, data.data());
+  q.enqueueWriteBuffer(buffer_in,CL_TRUE,0,num_dims * sizeof(int),input.data());
+
+  printf( "|--------------------------------+-------------------------|\n"
+          "| Kernel                         |    Wall-Clock Time (ns) |\n"
+          "|--------------------------------+-------------------------|\n");
+
+  // set kernel parameters
+  cl::Kernel kernel(program, "nearest_neighbor");
+  kernel.setArg(0,buffer_out);
+  kernel.setArg(1,buffer_points);
+  kernel.setArg(2,buffer_in);
+  kernel.setArg(3,num_points);
+  kernel.setArg(4,num_dims);
+
+  cl::Event event;
+  uint64_t nstimestart, nstimeend;
+  q.enqueueTask(kernel,NULL,&event);
+  q.finish();
+  event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_START,&nstimestart);
+  event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_END,&nstimeend);
+  auto simple_time = nstimeend-nstimestart;
+  printf("| %-30s | %23lu |\n", "Nearest Neighbor: simple", simple_time);
+
+  q.enqueueReadBuffer(buffer_out, CL_TRUE, 0, num_dims * sizeof(int), out.data());
+  verify(gold, out);
+
+  // set kernel parameters
+  cl::Kernel kernel_loop(program, "nearest_neighbor_loop_fusion");
+  kernel_loop.setArg(0,buffer_out);
+  kernel_loop.setArg(1,buffer_points);
+  kernel_loop.setArg(2,buffer_in);
+  kernel_loop.setArg(3,num_points);
+  kernel_loop.setArg(4,num_dims);
+
+  q.enqueueTask(kernel_loop,NULL,&event);
+  q.finish();
+  event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_START,&nstimestart);
+  event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_END,&nstimeend);
+  auto loop_time = nstimeend-nstimestart;
+  printf("| %-30s | %23lu |\n", "Nearest Neighbor: loop fusion", loop_time);
+
+  q.enqueueReadBuffer(buffer_out, CL_TRUE, 0, num_dims * sizeof(int), out.data());
+
+  verify(gold, out);
+  printf("|--------------------------------+-------------------------|\n");
+  printf("Note: Wall Clock Time is meaningful for real hardware execution only, not for emulation.\n");
+  printf("Please refer to profile summary for kernel execution time for hardware emulation.\n");
+  printf("TEST PASSED\n");
+  return EXIT_SUCCESS;
+}
+
+void find_nearest_neighbor(vector<int> &out, const int dim,
+                           const vector<int> &search_points,
+                           const vector<int> &points, const int num_points) {
+  // points is the list of data points that need to be searched for the given
+  // point (x, y)
+  //
+  // output array out has two values - the closest point to given input point
+  int best_i = 0;
+  int best_dist = numeric_limits<int>::max();
+
+  for (int p = 0; p < num_points; ++p) {
+    int dist = 0;
+
+    for (int c = 0; c < dim; ++c) {
+      int dx = abs(points[dim * p + c] - search_points[c]);
+      dist += dx * dx;
+    }
+
+    if (dist < best_dist) {
+      best_i = p;
+      best_dist = dist;
+    }
+  }
+
+  out.resize(dim);
+  for (int c = 0; c < dim; ++c) {
+    out[c] = points[best_i * dim + c];
+  }
+}
