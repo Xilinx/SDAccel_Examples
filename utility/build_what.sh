31e9a781fb8861bd84e5096d4cf583b2a7fe81d1
Spenser Gilliland <spenser@gillilanding.com>
2017-06-13 14:45:12 -0700

    build_what.sh: minor bug fixes

--- utility/build_what.sh
+++ utility/build_what.sh
@@ -12,19 +12,24 @@ REBUILDS=
 
 # Ignore the following patterns these are checked by the pre-check scripts
 for change in $CHANGES; do
-	if [[ "$change" == */README.md 
-		|| "$change" == "utility/build_what.sh" ]]; then
+	if [[ "$change" == */README.md
+		|| "$change" == "utility/build_what.sh"
+		|| "$change" == "Jenkinsfile" ]]; then
 		NUM_CHANGES=$((NUM_CHANGES-1))
 	fi
 done
 
 for change in $CHANGES; do
+	IN_PROJS=
 	for proj in $PROJS; do
 		if [[ "$change" == ${proj}* ]]; then
-			REBUILDS=$proj $REBUILDS
-			NUM_CHANGES=$((NUM_CHANGES-1))
+			IN_PROJS="$proj $IN_PROJS"
 		fi
 	done
+	if [[ "$IN_PROJS" != "" ]]; then
+		NUM_CHANGES=$((NUM_CHANGES-1))
+		REBUILDS="$IN_PROJS $REBUILDS"
+	fi
 done
 
 # if we know that we only changed something inside a single example then do a rebuild
