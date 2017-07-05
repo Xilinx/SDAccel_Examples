0b02e5b809ce282591baec9973c217891c0ec1f1
Heera Nand <heeran@xilinx.com>
2017-07-05 14:57:50 +0530

    Updated host global example for issue #200

--- getting_started/misc/host_global_bandwidth/src/host.cpp
+++ getting_started/misc/host_global_bandwidth/src/host.cpp
@@ -27,247 +27,143 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **********/
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <unistd.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <CL/opencl.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+#include <vector>
 
 #include "xcl.h"
 
-int main(int argc, char** argv) {
+////////////////////////////////////////////////////////////////////////////////
+class Timer {
+    std::chrono::high_resolution_clock::time_point mTimeStart;
+public:
+    Timer() {
+        reset();
+    }
+    long long stop() {
+        std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(timeEnd - mTimeStart).count();
+    }
+    void reset() {
+        mTimeStart = std::chrono::high_resolution_clock::now();
+    }
+};
 
-    xcl_world world = xcl_world_single();
-    clReleaseCommandQueue(world.command_queue);
+
+static int host_to_dev(cl_command_queue commands, int buff_size, std::vector<cl_mem> &mems, std::ostream &strm)
+{
+    cl_int err = 0;
+    Timer timer;
+    err = clEnqueueMigrateMemObjects(commands, mems.size(), &mems[0], 0, 0, NULL, NULL);
+    if (err != CL_SUCCESS) {
+        return err;
+    }
+    err = clFinish(commands);
+    if (err != CL_SUCCESS) {
+        return err;
+    }
+
+    double timer_stop2 = timer.stop();
+    double throput = (double)(buff_size * mems.size());
+    throput *= 1000000; // convert us to s;
+    throput /= 1024 * 1024; // convert to MB
+    throput /= timer_stop2;
+    std::cout << "OpenCL migration BW host to device: " << throput << " MB/s" <<" for buffer size "<< buff_size << " with " << mems.size()<< " buffers\n";
+    strm << "Host to Card, " << buff_size << ", " << mems.size() << ", " << throput << "\n";
+    return CL_SUCCESS;
+}
+
+static int dev_to_host(cl_command_queue commands, int buff_size, std::vector<cl_mem> &mems, std::ostream &strm)
+{
+    cl_int err = 0;
+    Timer timer;
+    err = clEnqueueMigrateMemObjects(commands, mems.size(), &mems[0], CL_MIGRATE_MEM_OBJECT_HOST, 0, NULL, NULL);
+    if (err != CL_SUCCESS) {
+        return err;
+    }
+    err = clFinish(commands);
+    if (err != CL_SUCCESS) {
+        return err;
+    }
+
+    long long timer_stop2 = timer.stop();
+    double throput = (double)(buff_size * mems.size());
+    throput *= 1000000; // convert us to s;
+    throput /= 1024 * 1024; // convert to MB
+    throput /= timer_stop2;
+    std::cout << "OpenCL migration BW device to host: " << throput << " MB/s" <<" for buffer size " << buff_size << " with " << mems.size()<< " buffers\n";
+    strm << "Card to Host, " << buff_size << ", " << mems.size() << ", " << throput << "\n";
+    return CL_SUCCESS;
+}
+
+int main(int argc, char** argv)
+{
+    //Variable-------------------------------------------------------------------------------
+
+    int buff_tab[][2] = {{64,1024},
+                         {256,1024},
+                         {512,1024},
+                         {1024,1024},
+                         {4096,1024},
+                         {16384,512},
+                         {1048576,8},
+                         {1048576,64},
+                         {1048576,256},
+                         {2097152,8},
+                         {2097152,64},
+                         {2097152,256},
+                         {16777216,64},
+                         {268435456,4},
+                         {536870912,2}};
 
     int err;
-    world.command_queue = clCreateCommandQueue(world.context, world.device_id,
-                                               CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_PROFILING_ENABLE,
-                                               &err);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to create a command queue!\n");
-        exit(EXIT_FAILURE);
-    }
+    xcl_world world     = xcl_world_single();
+    cl_program program  = xcl_import_binary(world,"krnl_host_global");
 
-    cl_program program = xcl_import_binary(world, "krnl_host_global");
-    cl_kernel krnl = xcl_get_kernel(program, "bandwidth");
+    const int dim1 = sizeof(buff_tab)/(2 * 4);
 
-    size_t globalbuffersize = 1024 * 1024 * 1024;  // 1 GB
+    std::ofstream handle("metric1.csv");
+    handle << "Direction, Buffer Size (bytes), Count, Bandwidth (MB/s)\n";
+    for(int buff_size_1 = 0; buff_size_1 < dim1; buff_size_1++) {
+        int nxtcnt = buff_tab[buff_size_1][0];
+        int buff_cnt = buff_tab[buff_size_1][1];
+        std::vector<cl_mem> mems(buff_cnt, 0);
+        for(int i=mems.size() - 1; i>=0; i--){
+            mems[i] = clCreateBuffer(world.context, CL_MEM_READ_WRITE, nxtcnt, NULL, NULL);
+            err = clEnqueueFillBuffer(world.command_queue, mems[i], &i, 4, 0, nxtcnt, 0, 0, 0);
+            if (err != CL_SUCCESS) {
+                break;
+            }
+        }
+        if (err != CL_SUCCESS) {
+            break;
+        }
 
-    /* Reducing the data size for emulation mode */
-    char *xcl_mode = getenv("XCL_EMULATION_MODE");
-    if (xcl_mode != NULL){
-        globalbuffersize = 1024 * 1024;  /* 1MB */
-    }
+        err = clFinish(world.command_queue);
+        if (err != CL_SUCCESS) {
+            break;
+        }
 
-    //--------------------------------------------------------------------------
-    //Measure PCIe read bandwidth (clEnqueueMapBuffer)
-    //--------------------------------------------------------------------------
-    char* map_buffer0;
-    char* map_buffer1;
+        err = host_to_dev(world.command_queue, nxtcnt, mems, handle);
+        if (err != CL_SUCCESS) {
+            break;
+        }
 
-    // Create buffers in the context
-    cl_mem dev_buffer0 = xcl_malloc(world, CL_MEM_READ_WRITE, globalbuffersize);
-    cl_mem dev_buffer1 = xcl_malloc(world, CL_MEM_READ_WRITE, globalbuffersize);
+        err = dev_to_host(world.command_queue, nxtcnt, mems, handle);
+        if (err != CL_SUCCESS) {
+            break;
+        }
 
-    // Copy test data into the buffers
-    char* buffer0 = (char*) malloc(globalbuffersize);
-    char* buffer1 = (char*) malloc(globalbuffersize);
-
-    // Set all bits to an alternating pattern of 10 and 01
-    memset(buffer0, 0xAA, globalbuffersize);
-    memset(buffer1, 0x55, globalbuffersize);
-    xcl_memcpy_to_device(world, dev_buffer0, buffer0, globalbuffersize);
-    xcl_memcpy_to_device(world, dev_buffer1, buffer1, globalbuffersize);
-
-    free(buffer0);
-    free(buffer1);
-
-    // Migrate buffers to device by running a kernel on the device
-    xcl_set_kernel_arg(krnl, 0, sizeof(cl_mem), &dev_buffer0);
-    xcl_set_kernel_arg(krnl, 1, sizeof(cl_mem), &dev_buffer1);
-    xcl_run_kernel3d(world, krnl, 1, 1, 1);
-
-
-    cl_event event0, event1;
-    // Map buffer so it is visible on the host
-    map_buffer0 = (char*) clEnqueueMapBuffer(world.command_queue,
-                                             dev_buffer0,
-                                             CL_FALSE,
-                                             CL_MAP_READ | CL_MAP_WRITE,
-                                             0,
-                                             globalbuffersize,
-                                             0,
-                                             NULL,
-                                             &event0,
-                                             &err);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clEnqueueMapBuffer OpenCL buffer\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-    clFinish(world.command_queue);
-
-    unsigned long nsduration = xcl_get_event_duration(event0);
-    double dnsduration = nsduration;
-    double dsduration = dnsduration / (double) 1000000000.0;
-    double dmbytes = (double) globalbuffersize / (((double)1024) * ((double)1024));
-    double dmbpersec = (dmbytes/dsduration);
-
-    printf("PCIe read only:%.1lf MB, duration %lf sec, bandwidth %lf MB/sec\n", dmbytes, dsduration, dmbpersec);
-
-    //--------------------------------------------------------------------------
-    //Measure PCIe write bandwidth (clEnqueueUnmapMemObject)
-    //--------------------------------------------------------------------------
-
-    err = clEnqueueUnmapMemObject(world.command_queue,
-                                  dev_buffer0,
-                                  map_buffer0,
-                                  0,
-                                  NULL,
-                                  &event0);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clEnqueueUnmapMemObject OpenCL buffer\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-    clFinish(world.command_queue);
-
-    nsduration = xcl_get_event_duration(event0);
-    dnsduration = nsduration;
-    dsduration = dnsduration / (double) 1000000000.0;
-    dmbytes = (double) globalbuffersize / (((double)1024) * ((double)1024));
-    dmbpersec = (dmbytes/dsduration);
-
-    printf("PCIe write only:%.1lf MB, duration %lf sec, bandwidth %lf MB/sec\n", dmbytes, dsduration, dmbpersec);
-
-    //--------------------------------------------------------------------------
-    //Measure concurrent PCIe write/read bandwidth (clEnqueueMapBuffer + clEnqueueUnmapMemObject)
-    //--------------------------------------------------------------------------
-    
-    // Map buffer0 for PCIe write
-    map_buffer0 = (char*) clEnqueueMapBuffer(world.command_queue,
-                                             dev_buffer0,
-                                             CL_FALSE,
-                                             CL_MAP_READ | CL_MAP_WRITE,
-                                             0,
-                                             globalbuffersize,
-                                             0,
-                                             NULL,
-                                             &event0,
-                                             &err);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clEnqueueMapBuffer OpenCL buffer\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-    clFinish(world.command_queue);
-
-    //unmap buffer0 (PCIe write buffer0)
-    err = clEnqueueUnmapMemObject(world.command_queue,
-                                  dev_buffer0,
-                                  map_buffer0,
-                                  0,
-                                  NULL,
-                                  &event0);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clEnqueueUnmapMemObject OpenCL buffer\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-
-    // map buffer1 (PCIe read buffer1) in parallel to unmap buffer0
-    map_buffer1 = (char*) clEnqueueMapBuffer(world.command_queue,
-                                             dev_buffer1,
-                                             CL_FALSE,
-                                             CL_MAP_READ | CL_MAP_WRITE,
-                                             0,
-                                             globalbuffersize,
-                                             0,
-                                             NULL,
-                                             &event1,
-                                             &err);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clEnqueueMapBuffer OpenCL buffer\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-    clFinish(world.command_queue);
-
-    unsigned long nstimequeued, nstimequeued0, nstimequeued1, nstimeend, nstimeend0, nstimeend1;
-
-    //map buffer1 and unmap buffer0 are completed.
-    //get profiling info for unmap buffer0
-    err = clGetEventProfilingInfo(event0, CL_PROFILING_COMMAND_START, sizeof(uint64_t),((void *)(&nstimequeued0)),NULL);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clGetEventProfilingInfo\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-
-    err = clGetEventProfilingInfo(event0, CL_PROFILING_COMMAND_END,    sizeof(uint64_t),((void *)(&nstimeend0)),   NULL);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clGetEventProfilingInfo\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-
-    // Get profiling info for map buffer1
-    err = clGetEventProfilingInfo(event1, CL_PROFILING_COMMAND_START, sizeof(uint64_t), ((void *)(&nstimequeued1)), NULL);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clGetEventProfilingInfo\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-
-    err = clGetEventProfilingInfo(event1, CL_PROFILING_COMMAND_END,    sizeof(uint64_t), ((void *)(&nstimeend1)),    NULL);
-    if (err != CL_SUCCESS) {
-        printf("Error: Failed to clGetEventProfilingInfo\n");
-        printf("Error: Test failed\n");
-        return EXIT_FAILURE;
-    }
-
-    if (nstimequeued0 < nstimequeued1)
-        nstimequeued = nstimequeued0;
-    else
-        nstimequeued = nstimequeued1;
-
-    if (nstimeend0 < nstimeend1)
-        nstimeend = nstimeend1;
-    else
-        nstimeend = nstimeend0;
-
-    nsduration = nstimeend - nstimequeued;
-    dnsduration = nsduration;
-    dsduration = dnsduration / ((double) 1000000000);
-    dmbytes = globalbuffersize / (((double)1024) * ((double)1024));
-    dmbpersec = (dmbytes/dsduration);
-
-    printf("PCIe concurrent write/read:%.1lf MB, duration %lf sec, bandwidth %lf MB/sec\n", dmbytes, dsduration, dmbpersec);
-  
-    for(size_t i = 0; i < globalbuffersize; i++) {
-        if(map_buffer0[i] != (char) 0xAA || map_buffer1[i] != (char) 0x55) {
-            printf("ERROR: Failed to validate data transfers: %lu %02x %02x\n", i , map_buffer0[i], map_buffer1[i]);
-            exit(EXIT_FAILURE);
+        for (cl_mem m : mems){
+            clReleaseMemObject(m);
         }
     }
 
-    // Free memory for all objects
-    clReleaseEvent(event0);
-    clReleaseEvent(event1);
-    clReleaseMemObject(dev_buffer0);
-    clReleaseMemObject(dev_buffer1);
-    clReleaseKernel(krnl);
+    // Shutdown and cleanup
+    handle.close();
     clReleaseProgram(program);
     xcl_release_world(world);
-
-    printf("------------------------------------------------------\n");
-    printf("TEST PASSED\n");
-    return EXIT_SUCCESS;
 }
