bb933d6bdb82e90ef371977905370eb0cc36585b
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2018-03-13 17:10:57 +0530

    helloworld example for HLS C Kernel

---
+++ getting_started/host/helloworld_c/src/host.cpp
@@ -0,0 +1,121 @@
+/**********
+Copyright (c) 2018, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+
+#include "xcl2.hpp"
+#include <vector>
+
+#define DATA_SIZE 4096
+
+int main(int argc, char** argv)
+{
+    size_t vector_size_bytes = sizeof(int) * DATA_SIZE;
+    // Allocate Memory in Host Memory
+    // When creating a buffer with user pointer (CL_MEM_USE_HOST_PTR), under the hood user ptr 
+    // is used if it is properly aligned. when not aligned, runtime had no choice but to create
+    // its own host side buffer. So it is recommended to use this allocator if user wish to
+    // create buffer using CL_MEM_USE_HOST_PTR to align user buffer to page boundary. It will 
+    // ensure that user buffer is used when user create Buffer/Mem object with CL_MEM_USE_HOST_PTR 
+    std::vector<int,aligned_allocator<int>> source_in1(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> source_in2(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> source_hw_results(DATA_SIZE);
+    std::vector<int,aligned_allocator<int>> source_sw_results(DATA_SIZE);
+
+    // Create the test data 
+    for(int i = 0 ; i < DATA_SIZE ; i++){
+        source_in1[i] = rand() % DATA_SIZE;
+        source_in2[i] = rand() % DATA_SIZE;
+        source_sw_results[i] = source_in1[i] + source_in2[i];
+        source_hw_results[i] = 0;
+    }
+
+// OPENCL HOST CODE AREA START
+    // get_xil_devices() is a utility API which will find the xilinx
+    // platforms and will return list of devices connected to Xilinx platform
+    std::vector<cl::Device> devices = xcl::get_xil_devices();
+    cl::Device device = devices[0];
+
+    cl::Context context(device);
+    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+    std::string device_name = device.getInfo<CL_DEVICE_NAME>(); 
+
+    // find_binary_file() is a utility API which will search the xclbin file for
+    // targeted mode (sw_emu/hw_emu/hw) and for targeted platforms.
+    std::string binaryFile = xcl::find_binary_file(device_name,"vadd");
+
+    // import_binary_file() ia a utility API which will load the binaryFile
+    // and will return Binaries.
+    cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
+    devices.resize(1);
+    cl::Program program(context, devices, bins);
+    cl::Kernel krnl_vector_add(program,"vadd");
+
+    // Allocate Buffer in Global Memory
+    // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and 
+    // Device-to-host communication
+    cl::Buffer buffer_in1   (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
+            vector_size_bytes, source_in1.data());
+    cl::Buffer buffer_in2   (context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, 
+            vector_size_bytes, source_in2.data());
+    cl::Buffer buffer_output(context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, 
+            vector_size_bytes, source_hw_results.data());
+
+    // Copy input data to device global memory
+    q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0/* 0 means from host*/);
+
+    int size = DATA_SIZE;
+    krnl_vector_add.setArg(0, buffer_in1);
+    krnl_vector_add.setArg(1, buffer_in2);
+    krnl_vector_add.setArg(2, buffer_output);
+    krnl_vector_add.setArg(3, size);
+
+    // Launch the Kernel
+    // For HLS kernels global and local size is always (1,1,1). So, it is recommended
+    // to always use enqueueTask() for invoking HLS kernel
+    q.enqueueTask(krnl_vector_add);
+
+    // Copy Result from Device Global Memory to Host Local Memory
+    q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST);
+    q.finish();
+// OPENCL HOST CODE AREA END
+
+    // Compare the results of the Device to the simulation
+    bool match = true;
+    for (int i = 0 ; i < DATA_SIZE ; i++){
+        if (source_hw_results[i] != source_sw_results[i]){
+            std::cout << "Error: Result mismatch" << std::endl;
+            std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
+                << " Device result = " << source_hw_results[i] << std::endl;
+            match = false;
+            break;
+        }
+    }
+
+    std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl; 
+    return (match ? EXIT_SUCCESS : EXIT_FAILURE);
+}
