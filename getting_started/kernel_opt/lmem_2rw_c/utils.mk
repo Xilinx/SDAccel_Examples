354e68a0b3a6d67c2c1c8fe683335ceab01cde77
Rajat Sarkari <rajat.sarkari@xilinx.com>
2018-08-25 10:04:41 +0530

    Added a check for XILINX_SDX environment variable in the utils.mk

--- getting_started/kernel_opt/lmem_2rw_c/utils.mk
+++ getting_started/kernel_opt/lmem_2rw_c/utils.mk
@@ -24,8 +24,12 @@ ifeq ($(DEBUG), yes)
 CLFLAGS += --dk protocol:all:all:all
 endif
 
+#Checks for XILINX_SDX
+ifndef XILINX_SDX
+$(error XILINX_SDX variable is not set, please set correctly and rerun)
+endif
 
-# sanitize_dsa - create a filesystem friendly name from dsa name
+#   sanitize_dsa - create a filesystem friendly name from dsa name
 #   $(1) - name of dsa
 COLON=:
 PERIOD=.
