4d73351e3c36cdfb91a713c599c5757b76272620
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2018-11-12 11:02:54 +0530

    updated the watermarking example to latest auto_banking

--- vision/watermarking/src/watermarking.cpp
+++ vision/watermarking/src/watermarking.cpp
@@ -85,15 +85,16 @@ int main(int argc, char* argv[])
     devices.resize(1);
     OCL_CHECK(err, cl::Program program (context, devices, bins, NULL, &err));
     OCL_CHECK(err, cl::Kernel apply_watermark(program, "apply_watermark", &err));
+    cl_kernel krnl=apply_watermark.get();
 
     // For Allocating Buffer to specific Global Memory Bank, user has to use cl_mem_ext_ptr_t
-    // and provide the Banks
-    cl_mem_ext_ptr_t inExt={0}, outExt={0};  // Declaring two extensions for both buffers
-    inExt.flags  = XCL_MEM_DDR_BANK0; // Specify Bank0 Memory for input memory
-    outExt.flags = XCL_MEM_DDR_BANK1; // Specify Bank1 Memory for output Memory
+    cl_mem_ext_ptr_t inExt, outExt;  // Declaring two extensions for both buffers
+    inExt.flags  = 0; // argument index ( 0 means that this buffer will be passed to argument 0 of the kernel )
+    outExt.flags = 1; // argument index ( 1 means that this buffer will be passed to argument 1 of the kernel )
     inExt.obj 	 = inputImage.data();
-    outExt.obj   = outImage.data(); // Setting Param to Zero
-    inExt.param  = 0 ; outExt.param = 0;
+    outExt.obj   = outImage.data(); 
+    // Setting kernel handle to Param
+    inExt.param  = krnl ; outExt.param = krnl;
 
     // Allocate Buffer in Global Memory
     // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and
