8d0bdb350394c24e1dba853362d31e6f7c7789a8
Rajat Sarkari <rajat.sarkari@xilinx.com>
2019-05-24 12:56:24 +0530

    New example added for RTL K2K Stream use-case

---
+++ getting_started/rtl_kernel/rtl_streaming_k2k/config.mk
@@ -0,0 +1,7 @@
+VIVADO := $(XILINX_VIVADO)/bin/vivado
+$(XCLBIN)/myadder1.$(TARGET).$(DSA).xo:
+	mkdir -p $(XCLBIN)
+	$(VIVADO) -mode batch -source gen_xo_myadder1.tcl -tclargs $(DEVICE) $(XCLBIN)/myadder1.$(TARGET).$(DSA).xo
+$(XCLBIN)/myadder2.$(TARGET).$(DSA).xo:
+	mkdir -p $(XCLBIN)
+	$(VIVADO) -mode batch -source gen_xo_myadder2.tcl -tclargs $(DEVICE) $(XCLBIN)/myadder2.$(TARGET).$(DSA).xo
